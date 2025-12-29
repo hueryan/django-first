3703faedd74727e42cb89f4457a58cadfca8efc3
hueryan <hueryan2002@163.com>
2025-12-29 23:47:41 +0800

    write menu and playground scene

--- app/settings.py
+++ app/settings.py
@@ -17,7 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE = os.path.join(Path(__file__).resolve().parent, "settings.env")
-print(ENV_FILE)
 DJANGO_HOST = dotenv_values(ENV_FILE)["DJANGO_HOST"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,4 +1,13 @@
 class AGame{
     constructor(id) {
+        this.id = id;
+        this.$a_game = $('#' + id);
+        this.menu = new AGameMenu(this);
+        this.playground = new AGamePlayground(this);
+
+        this.start();
+    }
+
+    start() {
     }
 }
