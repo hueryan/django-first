2e5651c30bcfa4ab016742c96421935e0eaa849b
hueryan <hueryan2002@163.com>
2026-01-04 03:35:49 +0800

    Request user information

--- game/views/settings/acwing/web/receive_code.py
+++ game/views/settings/acwing/web/receive_code.py
@@ -1,6 +1,11 @@
 from django.shortcuts import redirect
 from django.core.cache import cache
 import requests
+from django.contrib.auth.models import User
+from game.models.player.player import Player
+from django.contrib.auth import login
+from random import randint
+
 
 def receive_code(request):
     data = request.GET
@@ -21,6 +26,30 @@ def receive_code(request):
     }
 
     access_token_res = requests.get(apply_access_token_url, params=params).json()
+    access_token = access_token_res['access_token']
+    openid = access_token_res['openid']
     # print(access_token_res)
+
+    players = Player.objects.filter(openid=openid)
+    if players.exists():  # 如果该用户已存在，则无需重新获取信息，直接登录即可
+        login(request, players[0].user)
+        return redirect('index')
+
+    get_userinfo_url = "https://www.acwing.com/third_party/api/meta/identity/getinfo/"
+    params = {
+        'access_token': access_token,
+        'openid': openid,
+    }
+    userinfo_res = requests.get(get_userinfo_url, params=params).json()
+    username = userinfo_res['username']
+    photo = userinfo_res['photo']
+
+    while User.objects.filter(username=username).exists():  # 找到一个新用户名
+        username += str(randint(0, 9))
+
+    user = User.objects.create(username=username)
+    player = Player.objects.create(user=user, photo=photo, openid=openid)  # 新建用户
+
+    login(request, user)
 
     return redirect('index')  # 返回 game/urls/index.py 中index
